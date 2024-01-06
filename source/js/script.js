2e95da3fdc1ccd4e3d2346fc8db03b37f67c5db2
Jpsern <tommy999_jps@yahoo.co.jp>
2024-01-07 00:03:25 +0900

    jQueryも必要ないので削除

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,10 @@
-(function($){
+(function(d){
   // Mobile nav
-  $(".mobile-nav-panel").click(function() {
-    $(".nav").toggleClass("active")
-  });
+  // $(".mobile-nav-panel").click(function() {
+  //   $(".nav").toggleClass("active")
+  // });
 
-})(jQuery);
+  d.querySelector(".mobile-nav-panel").addEventListener("click", function() {
+    d.querySelector(".nav").classList.toggle("active");
+  });
+})(document);
